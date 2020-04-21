b8e8753f343624fb69e337b177256c1bcf795c72
the-great-shazbot <wazandrew@gmail.com>
2020-04-21 10:20:52 -0400

    saving

--- oop_blackjack/game.py
+++ oop_blackjack/game.py
@@ -2,8 +2,73 @@ from oop_blackjack.decks import CasinoDeck
 from oop_blackjack.players import Dealer, Player
 
 
+# TODO: add flags for verbosity of game (eg. removing suites)
+
 class BlackJack:
     def __init__(self, num_decks=8):
         self.deck = CasinoDeck(num_decks)
         self.dealer = Dealer()
         self.player = Player()
+        self.playing = True
+
+    def main(self):
+        while self.playing:
+            # first deal
+            for card in self.deck.pick(2):
+                self.dealer.is_dealt(card)
+            for card in self.deck.pick(2):
+                self.player.is_dealt(card)
+
+            self.print_hand("Dealer", self.dealer.hand)
+            self.print_hand("Player", self.player.hand)
+
+            # blackjack check
+            d = True if self.dealer.hand.value == 21 else False
+            p = True if self.player.hand.value == 21 else False
+
+            if d and p:
+                BlackJack.line_print("End of Hand")
+                print("Push")
+                self.playing = self.fetch_continue()
+            elif d and not p:
+                BlackJack.line_print("End of Hand")
+                print("Dealer wins :(")
+                self.playing = self.fetch_continue()
+            elif p and not d:
+                BlackJack.line_print("End of Hand")
+                print("You win! :)")
+                self.playing = self.fetch_continue()
+
+            # no blackjack, now normal player functionality
+
+
+
+        # TODO: before new hand is dealt, do check for need to reshuffle (rebuild) deck
+
+    def fetch_continue(self):
+        cont_ = input('Continue? (Y/N):')
+        while cont_.lower() not in ["y", "n"]:
+            print('Please enter a valid response. `Y` or `N`')
+        if cont_.lower() == "y":
+            return True
+        return False
+
+    @staticmethod
+    def print_hand(who, hand):
+        """
+        fancy print Hand
+        :param who: Who's hand is being printed (eg. Player or Dealer)
+        :param hand: BlackJackHand class instance
+        """
+
+        BlackJack.line_print(f"{who}'s hand")
+        print(hand)
+        print(f'Value of {hand.value}')
+
+    @staticmethod
+    def line_print(text):
+        print(f"{'---------' * 3} {text}  {'---------' * 3}")
+
+
+if __name__ == '__main__':
+    BlackJack().main()
--- oop_blackjack/players.py
+++ oop_blackjack/players.py
@@ -6,26 +6,32 @@ from oop_blackjack.hands import BaseHand, BlackJackHand
 
 class BasePlayer(AbstractPlayer):
     def __init__(self):
-        self.hand = BaseHand()
+        self._hand = BaseHand()
 
     @property
     def hand(self):
         return self._hand
 
-    @hand.setter
-    def hand(self, value):
-        if not isinstance(value, AbstractHand):
-            raise ValueError('Player hand can only be set as an instance of `Hand`')
-        self._hand = value
+    # @hand.setter
+    # def hand(self, value):
+    #     if not isinstance(value, AbstractHand):
+    #         raise ValueError('Player hand can only be set as an instance of `Hand`')
+    #     self._hand = value
+
+    def is_dealt(self, card):
+        if not isinstance(card, AbstractCard):
+            raise TypeError('Can only deal Cards to Players')
+        self.hand.add(card)
 
 
 class Dealer(BasePlayer):
     def __init__(self):
         BasePlayer.__init__(self)
-        self.hand = BlackJackHand(dealer=True)
+        self._hand = BlackJackHand(dealer=True)
 
 
+# TODO: add chip/$$ count
 class Player(BasePlayer):
     def __init__(self):
         BasePlayer.__init__(self)
-        self.hand = BlackJackHand()
+        self._hand = BlackJackHand()
